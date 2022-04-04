144e7027cfa8e6e35224c1a736226201b2013891
zhangir-azerbayev <zazerbayev@gmail.com>
2022-04-04 15:56:44 -0400

    first problem

---
+++ src/train_algebra.lean
@@ -0,0 +1,19 @@
+import ring_theory.algebraic
+import tactic
+
+section basic 
+
+lemma normal_of_add_comm_group {A : Type*} [add_comm_group A] (H : add_subgroup A) : add_subgroup.normal H :=
+begin
+  sorry
+end
+
+end basic 
+
+section intermediate 
+
+end intermediate 
+
+section advanced 
+
+end advanced 

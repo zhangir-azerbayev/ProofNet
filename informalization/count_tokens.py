bfb851bde62e36bbb305deaeed58a2939d0fa192
zhangir-azerbayev <zazerbayev@gmail.com>
2022-07-07 18:38:08 -0400

    some utilities

--- informalization/count_tokens.py
+++ informalization/count_tokens.py
@@ -1,24 +1,24 @@
 import ndjson 
+import re
 from transformers import GPT2Tokenizer 
 
 tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-with open("raw_mathlib_data.json") as f: 
+with open("nl_code_pairs/v2.jsonl") as f: 
     data = ndjson.load(f)
 
 
-a = [x["nl_statement_of_codex"] for x in data][0:3]
+a = [x["nl_statement_of_codex"] for x in data]
 
-b = [x["formal_statement"] for x in data][0:3]
+b = [x["formal_statement"] for x in data]
 
-print(a)
-print(b)
 
-a_tokens = tokenizer(a)
+a_tokens = tokenizer(a, padding='do_not_pad')
 
-b_tokens = tokenizer(b)
+b_tokens = tokenizer(b, padding='do_not_pad')
 
-a_lens = sum(len(x["input_ids"]) for x in a_tokens)
-b_lens = sum(len(x["input_ids"]) for x in b_tokens)
 
-print(a_tokens + b_tokens)
+a_lens = sum(len(x) for x in a_tokens['input_ids'])
+b_lens = sum(len(x) for x in b_tokens['input_ids'])
+
+print(a_lens + b_lens)

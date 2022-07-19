58653a288d843d06ceb1a55030f0446e2cdec3a5
zhangir-azerbayev <zazerbayev@gmail.com>
2022-07-19 18:43:00 -0400

    a commit

--- informalization/docgen_export/example_parse.py
+++ informalization/docgen_export/example_parse.py
@@ -2,6 +2,18 @@ import json
 import sys
 import re 
 
+def merge_typestars_of_binders(binders): 
+    binders = [re.sub(r"Type u_[0-9]", "Type*", x) for x in binders]
+
+    i = 0 
+    for i in range(len(binders)-1):
+        if re.search(r"Type*", binders[i]) and re.search(r"Type*", binders[i+1]): 
+            binders[i] = binders[i][binders[i].index(":")]
+            
+
+
+
+
 def assemble_statement(kind, nm, binders, tp): 
     statement = kind + " " + nm 
     for binder in binders: 
@@ -16,7 +28,7 @@ def assemble_statement(kind, nm, binders, tp):
     return statement
 
 def process_ue_string(arg: str): 
-    print(arg)
+    #print(arg)
     arg = arg.replace("\n", " ")
     #print("ARG BEFORE PROCESSING: ", repr(arg))
     arg = re.sub(r"\ue000(.*?)\ue001", "", arg)
@@ -40,11 +52,12 @@ def parse_single_arg(arg):
 with open("docgen_export_full.json") as f:
     db = json.load(f)
 
-for x in db["decls"][:1]: 
-    print(json.dumps(x, indent=4))
+for x in db["decls"][:10]: 
+    #print(json.dumps(x, indent=4))
 
     list_of_args = [y["arg"] for y in x["args"]]
     binders = [parse_single_arg(y) for y in list_of_args]
     processed_tp = parse_single_arg(x["type"])
 
     print(assemble_statement(x["kind"], x["name"], binders, processed_tp))
+    print("\n")

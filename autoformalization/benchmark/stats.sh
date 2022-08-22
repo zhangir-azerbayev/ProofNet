db14c108b9c19c52f4404906db7b2a2fa6637a67
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2022-08-22 14:07:58 -0400

    fix

--- autoformalization/benchmark/stats.sh
+++ autoformalization/benchmark/stats.sh
@@ -1,4 +1,4 @@
-N_INFORMAL=`grep -r Exercise informal | wc -l`
+N_INFORMAL=`grep -r exercise informal | wc -l`
 N_FORMAL=`grep -r exercise formal | wc -l`
 
 echo "Number of informal exercices: $N_INFORMAL"

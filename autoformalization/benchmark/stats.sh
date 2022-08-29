ea2bd5daa2f47b545dd4e7aea1d13d3c5bb6b78d
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2022-08-29 11:59:41 -0400

    minor change in stats.sh

--- autoformalization/benchmark/stats.sh
+++ autoformalization/benchmark/stats.sh
@@ -2,10 +2,12 @@ for f in informal/*tex; do
     n=`grep 'Exercise ' $f | wc -l`
     echo $f: $n
 done
+echo
 for f in formal/*lean; do
     n=`grep 'exercise_' $f | wc -l`
     echo $f: $n
 done
+echo
 N_INFORMAL=`grep -r 'Exercise ' informal | wc -l`
 N_FORMAL=`grep -r exercise formal | wc -l`
 echo "Total number of informal exercices: $N_INFORMAL"

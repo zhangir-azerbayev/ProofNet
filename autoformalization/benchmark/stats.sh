cfbcd40935fff016e38b88ac6d237c459c8679e9
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2022-08-29 18:48:50 -0400

    minor

--- autoformalization/benchmark/stats.sh
+++ autoformalization/benchmark/stats.sh
@@ -8,8 +8,8 @@ for f in formal/*lean; do
     echo $f: $n
 done
 echo
-N_INFORMAL=`grep -r 'Exercise ' informal | wc -l`
-N_FORMAL=`grep -r exercise formal | wc -l`
+N_INFORMAL=`grep -r 'Exercise ' informal/*tex | wc -l`
+N_FORMAL=`grep -r exercise formal/*lean | wc -l`
 echo "Total number of informal exercices: $N_INFORMAL"
 echo "Total number of formalized exercices: $N_FORMAL"
 

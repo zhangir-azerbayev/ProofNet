76361f58e2663f25eeec2fa99f226b4c1b35f170
zhangir-azerbayev <zazerbayev@gmail.com>
2022-09-27 21:41:11 -0400

    added some artin

--- benchmark/formal/Artin.lean
+++ benchmark/formal/Artin.lean
@@ -4,18 +4,30 @@ import data.real.basic
 import data.complex.basic
 import data.fintype.basic
 import data.zmod.basic 
+import data.countable.basic
+import data.set.countable
+import data.polynomial.basic
+import group_theory.subgroup.basic
+import group_theory.quotient_group
+import group_theory.index 
+import group_theory.specific_groups.cyclic
+import group_theory.solvable 
+import group_theory.free_group
+import group_theory.presented_group
+import number_theory.zsqrtd.gaussian_int
+import ring_theory.ideal.operations
 
+open function fintype subgroup ideal polynomial submodule zsqrtd 
+open_locale big_operators
 noncomputable theory
 
--- How to talk about generators
 theorem exercise_2_2_9 {G : Type*} [group G] {a b : G} 
-    (h : a * b = b * a) : is_abelian (subgroup.generated G (a, b)) :=
+    (h : a * b = b * a) : 
+    ∀ x y : closure {x | x = a ∨ x = b}, x*y = y*x :=
 sorry
 
--- How to talk about isomorphisms from additive to mul group? 
-theorem exercise_2_3_1 {R : Type*} 
-    [ring R] (f : R →+* R) (hf : ∀ x : R, 0 < f x) :
-    is_mul_group_hom f :=
+-- this isn't quite correct, mult group of positive reals?
+theorem exercise_2_3_1 : multiplicative ℝ ≃ additive ℝ  :=
 sorry
 
 theorem exercise_2_3_2 {G : Type*} [group G] (a b : G) :
@@ -24,102 +36,108 @@ sorry
 
 theorem exercise_2_4_19 {G : Type*} [group G] {x : G} 
   (hx : order_of x = 2) (hx1 : ∀ y, order_of y = 2 → y = x) :
-  x ∈ subgroup.center G :=
+  x ∈ center G :=
 sorry
 
 -- is this ok? Are they actually equal in Lean?
 theorem exercise_2_8_6 {G H : Type*} [group G] [group H] :
-  subgroup.center (G × H) ≃* (subgroup.center G) × (subgroup.center H) :=
+  center (G × H) ≃* (center G) × (center H) :=
 sorry
 
+-- how to talk about ℝ+?
 theorem exercise_2_10_11 
     {G : Type*} [add_group G] {H : Type*} [add_group H] {f : G → H} 
     (hf : is_add_group_hom f) {g : H → G} (hg : is_add_group_hom g) :
     is_add_group_hom (g ∘ f) :=
 sorry
 
-theorem exercise_2_11_3 {G : Type*} [group G] 
-    (hG : even (card G)) : ∃ (g : G), g ≠ 1 ∧ g^2 = 1 :=
+theorem exercise_2_11_3 {G : Type*} [group G] [fintype G]
+    (hG : even (card G)) : ∃ x : G, order_of x = 2 :=
 sorry
 
 theorem exercise_3_2_7 {F : Type*} [field F] {G : Type*} [field G]
-    (f : F → G) (hf : is_ring_hom f) : injective f :=
+    (φ : F →+* G) : injective φ :=
 sorry
 
-theorem exercise_3_5_6 {V : Type*} [add_comm_group V] [vector_space ℂ V]
-    (hV : ∃ (s : set V), countable s ∧ span ℂ s = ⊤) :
-    ∃ (s : set V), countable s ∧ is_basis ℂ s :=
+theorem exercise_3_5_6 {K V : Type*} [field K] [add_comm_group V]
+    [module K V] {S : set V} (hS : set.countable S)
+    (hS1 : span K S = ⊤) {ι : Type*} (R : ι → V)
+    (hR : linear_independent K R) : countable ι := 
 sorry
 
-theorem exercise_3_7_2 {V : Type*} [vector_space ℂ V] 
-    (hV : ∀ (S : set V), finite S → ∃ (v : V), v ∉ ⋃₀ S) :
-    ∀ (S : set V), finite S → ∃ (v : V), v ∉ ⋃₀ S :=
+theorem exercise_3_7_2 {K V : Type*} [field K] [add_comm_group V]
+    [module K V] {ι : Type*} [fintype ι] (γ : ι → submodule K V) : 
+    (⋂ (i : ι), (γ i : set V)) ≠ ⊤ :=
 sorry
 
 theorem exercise_6_1_14 (G : Type*) [group G] 
-    (Z : set G) (hZ : is_subgroup Z) (hZc : is_center G Z) 
-    (hGZ : cyclic (quotient_group.quotient Z)) :
-    abelian G :=
+    (hG : is_cyclic $ G ⧸ (center G)) : 
+    center G = ⊤  :=
 sorry
 
-theorem exercise_6_4_2 {p q : ℕ} (hp : nat.prime p) 
-    (hq : nat.prime q) (h : p * q ≠ 1) :
-    ¬ simple_group (multiplicative (fin (p * q))) :=
+theorem exercise_6_4_2 {G : Type*} [group G] [fintype G] {p q : ℕ} 
+    (hp : prime p) (hq : prime q) (hG : card G = p*q) :
+    is_simple_group G → false :=
 sorry
 
-theorem exercise_6_4_3 {p q : ℕ} (hp : nat.prime p) 
-    (hq : nat.prime q) (h : p ^ 2 * q ≠ 1) :
-    ¬ simple_group (p ^ 2 * q) :=
+theorem exercise_6_4_3 {G : Type*} [group G] [fintype G] {p q : ℕ} 
+    (hp : prime p) (hq : prime q) (hG : card G = p^2 *q) :
+    is_simple_group G → false :=
 sorry
 
-theorem exercise_6_4_12 (G : Type*) [group G] (hG : card G = 224) :
-  ¬ simple_group G :=
+theorem exercise_6_4_12 {G : Type*} [group G] [fintype G] 
+    (hG : card G = 224) :
+    is_simple_group G → false :=
 sorry
 
 theorem exercise_6_8_1 {G : Type*} [group G] 
-    (a b : G) :
-    subgroup.generated G (a :: b :: []) = subgroup.generated G (b * a * b^2 :: b * a * b^3 :: []) :=
+    (a b : G) : closure ({a, b} : set G) = closure {b*a*b^2, b*a*b^3} :=
 sorry
 
-theorem exercise_6_8_4 : 
-    free_group 3 :=
+-- How to talk about free groups in the right way?
+theorem exercise_6_8_4 {α : Type*} [group α] [free_group α] (x y z : α): 
+    closure ({x,y,z} : set α) :=
 sorry
 
-theorem exercise_6_8_6 {G : Type*} [group G] 
-    (N : subgroup G) (hN : N.normal) (hG : is_cyclic G) 
-    (hGN : is_cyclic (G / N)) :
-    ∃ (g h : G), G = ⟨g, h⟩ :=
+theorem exercise_6_8_6 {G : Type*} [group G] (N : subgroup G) 
+    [N.normal] (hG : is_cyclic G) (hGN : is_cyclic (G ⧸ N)) :
+    ∃ (g h : G), closure ({g,h} : set G) = ⊤ :=
 sorry
 
-theorem exercise_10_1_13 {R : Type*} [ring R] (x : R) (hx : ∃ n : ℕ, x ^ n = 0) :
-    is_unit (1 + x) :=
+theorem exercise_10_1_13 {R : Type*} [ring R] {x : R} 
+    (hx : is_nilpotent x) : is_unit (1 + x) :=
 sorry
 
-theorem exercise_10_2_4 (x : polynomial ℤ) : 
-    (ideal.span {2}).inter (ideal.span {x}) = ideal.span {2 * x} :=
+theorem exercise_10_2_4 : 
+    span ({2} : set $ polynomial ℤ) ⊓ (span {X}) = 
+    span ({2 * X} : set $ polynomial ℤ) :=
 sorry
 
-theorem exercise_10_6_7 {I : ideal ℤ[i]} 
-    (hI : I ≠ ⊥) : ∃ (z : ℤ), z ≠ 0 ∧ z ∈ I :=
+theorem exercise_10_6_7 {I : ideal gaussian_int} 
+    (hI : I ≠ ⊥) : ∃ (z : I), z ≠ 0 ∧ (z : gaussian_int).im = 0 :=
 sorry
 
-theorem exercise_10_6_16 (f : polynomial ℤ) (a : ℤ) :
-  ∃ (g : polynomial ℤ), f = g.eval₂ (λ x y, x - y) a :=
+-- this is a thorny one
+theorem exercise_10_6_16 {R : Type*} [ring R] (x : polynomial R)
+    (a : R) :
+    ∃ n : ℕ, x = ∑ (i : fin n), :=
 sorry
 
-theorem exercise_10_3_24a {R : Type*} [comm_ring R] (I J : ideal R) :
-    ¬ (I.is_ideal ∧ J.is_ideal) → ¬ (I.is_ideal ∧ J.is_ideal ∧ (I ∪ J).is_ideal) :=
+-- problem of passing typeclass
+theorem exercise_10_3_24a : 
+    ∃ (R : Type*) [ring R] (I J: ideal R), :=
 sorry
 
 theorem exercise_10_4_6 {R : Type*} [comm_ring R] 
-    (I J : ideal R) (x : R) (hx : x ∈ I ∩ J) :
-  is_nilpotent (x + I J) :=
+    [no_zero_divisors R] (I J : ideal R) : 
+    ∀ x : I ⊓ J, is_nilpotent ((ideal.quotient.mk (I*J)) x) :=
 sorry
 
-theorem exercise_10_4_7a {R : Type*} [comm_ring R] 
-    (I J : ideal R) (hIJ : I + J = ⊤) : I * J = I ∩ J :=
+theorem exercise_10_4_7a {R : Type*} [comm_ring R] [no_zero_divisors R] 
+    (I J : ideal R) (hIJ : I + J = ⊤) : I * J = I ⊓ J :=
 sorry
 
+-- this is a tough one
 theorem exercise_10_5_16 {R : Type*} 
     [comm_monoid_with_zero R] (f : nat.arithmetic_function R) 
     (hf : f.is_multiplicative) (g : nat.arithmetic_function R) 
@@ -127,10 +145,9 @@ theorem exercise_10_5_16 {R : Type*}
     f = g ↔ ∀ (p i : ℕ), nat.prime p → f (p ^ i) = g (p ^ i) :=
 sorry
 
-theorem exercise_10_7_6 {α : Type*} 
-    [comm_ring α] [fintype α] [decidable_eq α] (p : polynomial α) 
-    (hp : irreducible p) :
-    is_field (polynomial.quotient p) :=
+theorem exercise_10_7_6 {F : Type*} [fintype F] [field F] 
+    (hF : card F = 5) : 
+    field $ (polynomial F) ⧸ ideal.span ({X^2 + X + 1} : set (polynomial F)) :=
 sorry
 
 theorem exercise_10_7_10 {R : Type*} [ring R] 
@@ -138,12 +155,12 @@ theorem exercise_10_7_10 {R : Type*} [ring R]
     is_maximal M ∧ ∀ (N : ideal R), is_maximal N → N = M :=
 sorry
 
-theorem exercise_11_2_13 {a b : ℤ} (h : a ∣ᵤ b) : a ∣ b :=
+theorem exercise_11_2_13 (a b : ℤ) :
+    (of_int a : gaussian_int) ∣ of_int b → a ∣ b :=
 sorry
 
-theorem exercise_11_3_1 {α : Type*} 
-    [field α] {a b : α} (h : a ≠ 0) (f : polynomial α) :
-    irreducible f ↔ irreducible (f.linear_substitution a b) :=
+theorem exercise_11_3_1 {F : Type*} [field F] (a b : F) (ha : a ≠ 0) (p : polynomial F) : 
+    irreducible p ↔ irreducible (∑ n in p.support, p.coeff n • (a • X + b • 1)^n : polynomial F) :=
 sorry
 
 theorem exercise_11_3_2 {F : Type*} 
@@ -152,25 +169,26 @@ theorem exercise_11_3_2 {F : Type*}
     ∃ (h : polynomial F), is_factor_of h f ∧ is_factor_of h g :=
 sorry
 
-theorem exercise_11_3_4 {R : Type*} [integral_domain R] 
-    [decidable_eq R] (f g : polynomial R) :
-    is_rel_prime f g ↔ ∃ (a : R), a ∈ ideal.span {f, g} :=
+theorem exercise_11_3_4 : irreducible (X^3 + 6*X + 12 : polynomial ℚ) :=
 sorry
 
-theorem exercise_11_4_1b : irreducible (polynomial.C 12 + 6 * X + X ^ 3) :=
+theorem exercise_11_4_1b {F : Type*} [field F] [fintype F] (hF : card F = 2) : 
+    irreducible (12 + 6 * X + X ^ 3 : polynomial F) :=
 sorry
 
-theorem exercise_11_4_6a : irreducible (polynomial.C (2 : ℤ)) (X^2 + X + 1) :=
+theorem exercise_11_4_6a {F : Type*} [field F] [fintype F] (hF : card F = 7) : 
+    irreducible (X ^ 2 + 1 : polynomial F) :=
 sorry
 
-theorem exercise_11_4_6b (x : ℤ/7) : irreducible (x^2 + 1) :=
+theorem exercise_11_4_6b {F : Type*} [field F] [fintype F] (hF : card F = 31) : 
+    irreducible (X ^ 3 - 9 : polynomial F) :=
 sorry
 
 theorem exercise_11_4_6c : irreducible (X^3 - 9 : polynomial ℤ_31) :=
 sorry
 
-theorem exercise_11_4_8 {p : ℕ} (hp : nat.prime p) (n : ℕ) :
-    irreducible (polynomial.X ^ n - C p) :=
+theorem exercise_11_4_8 {p : ℕ} (hp : prime p) (n : ℕ) :
+    irreducible (X ^ n - p : polynomial ℚ) :=
 sorry
 
 theorem exercise_11_4_10 irreducible_of_degree_two_n_plus_one_of_nonzero_leading_coeff_of_zero_coeffs_of_nonzero_constant_coeff_of_zero_coeffs_of_zero_coeffs_of_nonzero_constant_coeff_of_zero_coeffs_of_zero_coeffs_of_nonzero_constant_coeff_of_zero_coeffs_of_zero_coeffs_of_nonzero_constant_coeff_of_zero_coeffs_of_zero_coeffs_of_nonzero_constant :=
@@ -182,26 +200,27 @@ theorem exercise_11_9_4 {R : Type*}
     ideal.generated_by (p, α) = ideal.mul (ideal.generated_by p) (ideal.generated_by α) :=
 sorry
 
-theorem exercise_11_12_3 
-    (p : ℕ) (hp : nat.prime p) (h : ∃ (x : ℤ), x ^ 2 ≡ -5 [MOD p]) :
+theorem exercise_11_12_3 (p : ℕ) (hp : nat.prime p) {a : zmod p} 
+    (ha : a^2 = -5) :
     ∃ (x y : ℤ), x ^ 2 + 5 * y ^ 2 = p ∨ 2 * x ^ 2 + 2 * x * y + 3 * y ^ 2 = p :=
 sorry
 
-theorem exercise_11_13_3 :
-  ∃ (p : ℕ), nat.prime p ∧ p ≡ -1 [MOD 4] :=
+theorem exercise_11_13_3 (N : ℕ):
+    ∃ p ≥ N, nat.prime p ∧ p + 1 ≡ 0 [MOD 4] :=
 sorry
 
-theorem exercise_13_1_3 {R : Type*} 
-    [integral_domain R] [field F] (hF : subring F R) 
-    (hR : finite_dimensional F R) : is_field R :=
+-- Why is this erroring?
+theorem exercise_13_1_3 {R : Type*} [comm_ring R] [no_zero_divisors R] 
+    {F : subring R} [module F R] [finite_dimensional F R] : 
+    field R :=
 sorry
 
-theorem exercise_13_3_1 {F : Type*} [field F] 
-    {α : F} (hα : is_integral α) (hα2 : is_integral (α ^ 2)) 
-    (hα5 : degree F α = 5) (hα2_5 : degree F (α ^ 2) = 5) :
-    ∃ (β : F), is_integral β ∧ degree F β = 5 ∧ α = β ^ 2 :=
+-- Not really sure how to do this
+theorem exercise_13_3_1 {F : Type*} [field F] (f : polynomial F)
+    (hf : f.degree = 5)   :=
 sorry
 
+-- also not sure how to do this
 theorem exercise_13_3_8 {F : Type*} [field F] {K : Type*} 
     [field K] (hK : algebra.is_field_extension F K) (α : K) (β : K) 
     (hα : algebra.is_integral F α) (hβ : algebra.is_integral F β) 
@@ -210,11 +229,10 @@ theorem exercise_13_3_8 {F : Type*} [field F] {K : Type*}
 sorry
 
 theorem exercise_13_4_10 
-    (p : ℕ) (hp : nat.prime p) (h : p = 2 ^ p.nat_abs + 1) :
+    {p : ℕ} {hp : nat.prime p} (h : ∃ r : ℕ, p = 2 ^ r + 1) :
     ∃ (k : ℕ), p = 2 ^ (2 ^ k) + 1 :=
 sorry
 
-theorem exercise_13_6_10 {K : Type*} [field K] [fintype K] 
-    (hK : ¬ (∃ (x : K), x ≠ 0 ∧ x ≠ 1)) : 
-    ∏ (x : K), x ≠ 0 → x = -1 :=
+theorem exercise_13_6_10 {K : Type*} [field K] [fintype Kˣ] : 
+    ∏ (x : Kˣ), x = -1 :=
 sorry

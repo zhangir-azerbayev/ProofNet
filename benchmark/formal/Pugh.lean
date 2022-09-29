d16114d6cd4d1f85c0085ffd68133817f5cbc20c
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2022-09-29 17:51:21 -0400

    more in Pugh

--- benchmark/formal/Pugh.lean
+++ benchmark/formal/Pugh.lean
@@ -11,6 +11,7 @@ import analysis.specific_limits.normed
 import analysis.box_integral.basic
 import data.set.intervals.basic
 import topology.basic
+import topology.bases
 import topology.metric_space.basic
 import topology.instances.real
 import dynamics.ergodic.measure_preserving
@@ -72,3 +73,82 @@ theorem exercise_2_85
       ∀ p, ∃ (V₁ V₂ ∈ V), p ∈ V₁ ∧ p ∈ V₂ ∧ V₁ ≠ V₂ :=
 sorry
 
+theorem exercise_2_92 {α : Type*} [topological_space α]
+    {s : ℕ → set α}
+    (hs : ∀ i, is_compact (s i))
+    (hs : ∀ i, (s i).nonempty)
+    (hs : ∀ i, (s i) ⊃ (s (i + 1))) :
+    (⋂ i, s i).nonempty :=
+sorry
+
+theorem exercise_2_109
+  {M : Type*} [metric_space M]
+  (h : ∀ x y z : M, dist x z = max (dist x y) (dist y z)) :
+  totally_disconnected_space M :=
+sorry
+
+theorem exercise_2_126 {E : set ℝ}
+    (hE : ¬ set.countable E) : ∃ (p : ℝ), cluster_pt p (𝓟 E) :=
+sorry
+
+open topological_space
+
+theorem exercise_2_137
+  {M : Type*} [metric_space M] [separable_space M] [complete_space M]
+  {P : set M} (hP : is_closed P)
+  (hP' : is_closed P ∧ P = {x | cluster_pt x (𝓟 P)}) :
+    ∀ x ∈ P, ∀ n ∈ (𝓝 x), ¬ set.countable n :=
+sorry
+
+theorem exercise_3_1 {f : ℝ → ℝ}
+  (hf : ∀ x y, |f x - f y| ≤ |x - y| ^ 2) :
+  ∃ c, f = λ x, c :=
+sorry
+
+theorem exercise_3_4 (n : ℕ) :
+  tendsto (λ n, (sqrt (n + 1) - sqrt n)) at_top (𝓝 0) :=
+sorry
+
+theorem exercise_3_11a
+  {f : ℝ → ℝ} {a b x : ℝ}
+  (h1 : differentiable_within_at ℝ f (set.Ioo a b) x)
+  (h2 : differentiable_within_at ℝ (deriv f) (set.Ioo a b) x) :
+  ∃ l, tendsto (λ h, (f (x - h) - 2 * f x + f (x + h)) / h ^ 2) (𝓝 0) (𝓝 l)
+  ∧ deriv (deriv f) x = l :=
+sorry
+
+local notation `∞` := (⊤ : with_top ℕ)
+
+theorem exercise_3_18 {L : set ℝ} (hL : is_closed L) :
+    ∃ (f : ℝ → ℝ), (cont_diff ℝ ∞ f) ∧ ∀ (x : ℝ), f x = 0 ↔ x ∈ L :=
+sorry -- TODO range should be [0,1]
+
+
+theorem exercise_3_63a (p : ℝ) (f : ℕ → ℝ) (hp : p > 1)
+  (h : f = λ k, (1 : ℝ) / (k * (log k) ^ p)) :
+  ∃ l, tendsto f at_top (𝓝 l) :=
+sorry
+
+theorem exercise_3_63b (p : ℝ) (f : ℕ → ℝ) (hp : p ≤ 1)
+  (h : f = λ k, (1 : ℝ) / (k * (log k) ^ p)) :
+  ¬ ∃ l, tendsto f at_top (𝓝 l) :=
+sorry
+
+theorem exercise_4_15a {α : Type*}
+    (a b : ℝ) (F : set (ℝ → ℝ)) :
+    (∀ (x : ℝ) (ε > 0), ∃ (U ∈ (𝓝 x)),
+      (∀ (y z ∈ U) (f : ℝ → ℝ), f ∈ F → (dist (f y) (f z) < ε)))
+    ↔
+      ∃ (μ : ℝ → ℝ), ∀ (x : ℝ), (0 : ℝ) ≤ μ x ∧ tendsto μ (𝓝 0) (𝓝 0) ∧
+        (∀ (s t ∈ ℝ) (f : ℝ → ℝ), f ∈ F → |(f s) - (f t)| ≤ μ (|s - t|)) :=
+sorry
+
+theorem exercise_4_19 {M : Type*} [metric_space M]
+    [compact_space M] (A : set M) (hA : dense A) (δ : ℝ) (hδ : δ > 0) :
+    ∃ (A_fin : set M), A_fin ⊂ A ∧ set.finite A_fin ∧ ∀ (x : M), ∃ i ∈ A_fin, dist x i < δ :=
+sorry
+
+theorem exercise_5_2 {V : Type*} [normed_group V]
+    [normed_space ℂ V] {W : Type*} [normed_group W] [normed_space ℂ W] :
+    normed_space ℂ (continuous_linear_map V W) :=
+sorry

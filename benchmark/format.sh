b5536c4a4f77256cae13bae1933f1dd896007194
zhangir-azerbayev <zazerbayev@gmail.com>
2023-02-20 23:38:03 -0500

    cleaned up

--- benchmark/format.sh
+++
@@ -1,7 +0,0 @@
-
-sed 's/    /  /g' -i formal/*lean
-TMP=`mktemp`
-for f in formal/*lean; do
-    cat --squeeze-blank $f > $TMP
-    mv $TMP $f
-done
--- benchmark/list_not_formalized.sh
+++
@@ -1,7 +0,0 @@
-
-for informal in informal/*tex; do
-    formal=formal/`basename -s .tex $informal`.lean
-    echo $informal
-    ./list_not_formalized.py $formal $informal
-    echo
-done
